652a5a63fd9de9a1e9da3b99012c9e03c413b8b7
github username <github email address>
2015-10-21 18:19:27 -0400

    debugged

--- jukebox.rb
+++ jukebox.rb
@@ -1,10 +1,16 @@
-def jukebox(command)
-  if command.downcase == "list"
-    list_library
-  else
-    parse_command(command)
-  end
+require_relative "./song_library.rb"
+require "pry"
+def list_artist(artist, album_hash)
+   artist_list = "\n---------------\n"
+   artist_list += "#{artist}:\n"
+   artist_list += "---------------"
+   album_hash[:albums].each do |album_name, songs_hash|
+     artist_list += "\n#{album_name}:\n\t"
+     artist_list += songs_hash[:songs].join("\n\t")
+   end
+   artist_list
 end
+
 
 def list_library
   lib = full_library
@@ -20,14 +26,15 @@ end
 def parse_artist(command, lib)
   cmd = command.to_sym
   parsed = false
+  # binding.pry
   if lib.has_key?(cmd)
     puts list_artist(command, lib[cmd])
-    parsed = false
+    parsed = true
   else
     lib.each do |artist, hash|
       if command.downcase == artist.to_s.gsub("_"," ").downcase
         puts list_artist(artist, lib)
-        parsed = true
+        parsed = false
         break
       end
     end
@@ -39,12 +46,12 @@ def play_song(command, lib)
   lib.each do |artist, hash|
     hash.each do |album_name, albums_hash|
       albums_hash.each do |album, songs_hash|
-        songs_hash.each do |songs|
-          songs.each do |song|
+        songs_hash[:songs].each do |song|
             if song.downcase == command.downcase
-            puts "Now Playing: #{artist[command].strip}: #{album} - #{song}\n\n"
-            return true
-          end
+              # binding.pry
+              puts "Now Playing: #{artist}: #{album} - #{song}\n\n"
+              return true
+            end
         end
       end
     end
@@ -52,19 +59,28 @@ def play_song(command, lib)
   false
 end
 
-def list_artist(artist, album_hash)
-   artist_list = "\n---------------\n"
-   artist_list += "#{artist}:\n"
-   artist_list += "---------------"
-   album_hash[:albums].each do |album_name, songs_hash|
-     artist_list += "\n#{album_name}:\n\t"
-     artist_list += songs_hash[:songs].join("\n\t")
-   end
-   artist_list
-end
 
 def not_found(command)
   puts "I did not understand '#{command}'!\n\n"
   true
 end
+
+def titlize(command)
+  command = command.split(" ").map {|x| x.capitalize}
+  ignore = ["and", "the", "in", "is", "a", "or", "of"]
+  for i in 1...command.length
+    if ignore.include?(command[i])
+      command[i] = command[i].downcase
+    end
+  end
+  # binding.pry
+  return command.join(" ")
+end
+
+def jukebox(command)
+  if command.downcase == "list"
+    list_library
+  else
+    parse_command(titlize(command))
+  end
 end
